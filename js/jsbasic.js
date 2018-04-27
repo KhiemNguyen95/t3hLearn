3a01eed751930adbfb3394d798c431a42e6f0630
Khiem Nguyen <nkhiem5@gmail.com>
2018-04-27 21:22:10 +0700

    learn

---
+++ js/jsbasic.js
@@ -0,0 +1,69 @@
+// document.write('hello world');
+// document.write("<h2>Khiem</h2>");
+// // alert('hello you');
+
+// // let name = prompt('name');
+// // console.log(name);
+
+// var av = 12;
+// var b; //undefined
+// console.log(typeof(av));
+// console.log(typeof(b));
+// var c = '12';
+
+// var str1 = "chuoi 1";
+// var str2 = "chuoi 2";
+// var str3 = str1 + str2;
+// var d = true;
+
+// if(d == 1)
+// {
+// 	console.log('Yes');	
+// }
+// {
+// 	console.log('No');
+// }
+
+// function testVar(){
+// 	var x = 1;
+// 	if(true)
+// 	{
+// 		x = 2;
+// 	}
+// 	console.log(x) 
+// }
+// //console.log(x) // error
+
+
+// function testLet(){
+// 	let x = 1;
+// 	if(true)
+// 	{
+// 		let x = 2;
+// 		console.log(x);
+// 	}
+// 	console.log(x); 
+// }
+
+// testLet();
+
+// let checkLet = 1;
+// //let checkLet = 0; // error
+
+// for (var i = 0; i <=10; i++) 
+// 	{
+// 		console.log(i);
+// 	}
+
+let a = 1;
+let b = 1;
+
+let c;
+ 
+ c = ++a + 2;
+ console.log("c"+c);
+ console.log("a" +a);
+
+ c = b++ + 2;
+  console.log("c"+c);
+ console.log("b"+b);
