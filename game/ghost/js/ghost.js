d3d2c6086c3b2b91bbf1515e0b05e89cf5ee54de
Khiem <nkhiem5@gmail.com>
2018-06-10 20:23:07 +0700

    game

--- game/ghost/js/ghost.js
+++ game/ghost/js/ghost.js
@@ -1,4 +1,10 @@
 let time = 0;
+let audio = document.getElementsByTagName('audio')[0];
+// 
+function autoPlayAudio(){
+	audio.play();
+}
+// 
 function countTime(){	
 	// one time
 	// setTimeout(function() {
@@ -13,7 +19,9 @@ function countTime(){
 			clearInterval(count);
 			document.getElementById('mainFunny').style.display = 'block';
 			document.getElementsByClassName('container')[0].style.display = 'none';
+			audio.play();
 		}
 	}, 1000);
 }
 countTime();
+
