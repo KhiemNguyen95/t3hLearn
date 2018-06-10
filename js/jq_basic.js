8c5d5deab80e12809b13920d174ea7c53723fc24
Khiem <nkhiem5@gmail.com>
2018-06-10 21:05:17 +0700

    learn

--- js/jq_basic.js
+++ js/jq_basic.js
@@ -2,5 +2,26 @@
 $(function(){
 	// jquery code here
 
-	$('button[type="button"]')
+	$('button[type="button"]').click(function(){
+		let data = $('#title').attr('id');
+		let data1 = $('.text').prop('class'); 
+		let myClass = $('#title').hasClass('text');
+		$('#title').removeClass('text');
+		$('#title').addClass(':)');
+		$('#title').toggleClass('tog');
+		console.log(myClass);
+		console.log(data,data1);
+	});
+	// change img
+	$('#changeImg').click(function(){
+		$('img').attr('src','resources/image/K.png');
+	});
+
+	// change type
+	$('#changeType').click(function(){
+		// $('#input').attr('type','button');
+		$('#input').removeAttr('placeholder');
+		let value = $('#input').val().trim();
+		console.log(value);
+	});
 });
